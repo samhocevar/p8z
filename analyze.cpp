f27c24ff955828f6431988d98acf583c42b61073
Sam Hocevar <sam@hocevar.net>
2018-11-15 00:19:14 +0100

    Refactor analysis program.

--- analyze.cpp
+++ analyze.cpp
@@ -10,9 +10,25 @@
 // The PICO-8 1-byte charset (excluding "\n")
 std::string CHARSET = " 0123456789abcdefghijklmnopqrstuvwxyz!#%(){}[]<>+=/*:;.,~_";
 
-int best_streak(std::string const &input, int start_pos, std::string const &current_lut, std::unordered_set<char> const &already_excluded)
+struct context
 {
-    std::unordered_set<char> excluded = already_excluded;
+    context()
+    {
+        for (int i = 0; i < 256; ++i)
+            excluded.insert(i);
+        for (char c : CHARSET)
+            excluded.erase(c);
+    }
+
+    std::unordered_set<char> excluded;
+
+    std::string prefix;
+    std::string suffix;
+};
+
+int best_streak(std::string const &input, int start_pos, context const &ctx)
+{
+    std::unordered_set<char> excluded = ctx.excluded;
 
     // Skip excluded chars until we reach EOF or a non-excluded char
     int best_len = 0;
@@ -21,10 +37,10 @@ int best_streak(std::string const &input, int start_pos, std::string const &curr
         ++best_len;
 
     // Check that _all_ skipped characters match the current LUT
-    if (best_len > (int)current_lut.size())
+    if (best_len > (int)ctx.prefix.size())
         return 0;
     for (int i = 0; i < best_len; ++i)
-        if (current_lut[current_lut.size() - 1 - i] != input[start_pos + i])
+        if (ctx.prefix[ctx.prefix.size() - best_len + i] != input[start_pos + i])
             return 0;
 
     // Count how many more characters match
@@ -41,30 +57,26 @@ int main()//int argc, char *argv[])
     auto input = std::string{ std::istreambuf_iterator<char>(std::cin),
                               std::istreambuf_iterator<char>() };
 
-    std::string prefix = ",i";
-    std::string suffix = "do t[sub(";
+    context ctx;
+
+    ctx.prefix = ",i";
+    ctx.suffix = "do t[sub(";
 
     // Exclude characters that would be encoded as multibyte
-    std::unordered_set<char> excluded;
-    for (char c : input)
-        excluded.insert(c);
-    for (char c : CHARSET)
-        excluded.erase(c);
-    for (char c : prefix + suffix)
-        excluded.insert(c);
+    for (char c : ctx.prefix + ctx.suffix)
+        ctx.excluded.insert(c);
 
-    // Replace inline strings with """"""" sequences
+    // Replace inline strings with """""""… sequences so that
+    // our stats don't get messed up by the contents of that string.
     for (int pos = 0, in_string = 0; pos < (int)input.size(); ++pos)
     {
         ++frequencies[input[pos]];
 
         if (input[pos] == '"' && (pos == 0 || input[pos - 1] != '\\'))
             in_string = 1 - in_string;
-        if (in_string && !excluded.count(input[pos]))
+        if (in_string && !ctx.excluded.count(input[pos]))
             input[pos] = '"';
     }
-
-    std::string result = prefix;
 
     for (int iter = 0; iter < 10; ++iter)
     {
@@ -73,7 +85,7 @@ int main()//int argc, char *argv[])
 
         for (int pos = 0; pos < (int)input.size(); ++pos)
         {
-            int size = best_streak(input, pos, result, excluded);
+            int size = best_streak(input, pos, ctx);
 
             if (size < 3)
                 continue;
@@ -83,8 +95,8 @@ int main()//int argc, char *argv[])
                 score += 1.f / frequencies[input[pos + i]];
             score *= size - 2;
 
-            if (score > best_score)
-            //if (size > best_size)
+            //if (score > best_score)
+            if (size > best_size)
             {
                 best_score = score;
                 best_pos = pos;
@@ -96,22 +108,24 @@ int main()//int argc, char *argv[])
                best_score, best_pos, best_size, input.substr(best_pos, best_size).c_str());
 
         for (int i = 0; i < best_size; ++i)
-            if (!excluded.count(input[best_pos + i]))
+            if (!ctx.excluded.count(input[best_pos + i]))
             {
-                result += input.substr(best_pos + i, best_size - i).c_str();
+                ctx.prefix += input.substr(best_pos + i, best_size - i).c_str();
                 break;
             }
 
         for (int i = 0; i < best_size; ++i)
-            excluded.insert(input[best_pos + i]);
+            ctx.excluded.insert(input[best_pos + i]);
     }
 
+    std::string result = ctx.prefix;
+
     for (char c : CHARSET)
-        if (excluded.count(c) == 0)
+        if (ctx.excluded.count(c) == 0)
             result += c;
 
     // Handle suffix
-    result += suffix;
+    result += ctx.suffix;
 
     printf("Final string: \"%s\"\n", result.c_str());
 }
